40a6dfe60ada869393befcbba68fca39bf424569
ktcotz <naskret.kamil@gmail.com>
2024-12-26 18:14:39 +0100

    feat: optimizations,robots,lazy loading
    Work around optimizations page, meta descriptions to better
    SEO, work with lazy loading of react router imports.
    Learn about production build on NX and add serve to serving dist.

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
-import { Root } from './pages/Root';
+import { lazy, Suspense } from 'react';
+
+const Root = lazy(async () => {
+  const module = await import('./pages/Root.tsx');
+  return { default: module.Root };
+});
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Root />} />
-    </Routes>
+    <Suspense fallback={<p>Loading...</p>}>
+      <Routes>
+        <Route path="/" element={<Root />} />
+      </Routes>
+    </Suspense>
   );
 };
